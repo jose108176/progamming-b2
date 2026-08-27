87bcf863cc5740423ba932f085a4fb589ca00e11
jose108176 <jose108176@mcpsva.org>
2026-08-27 11:01:07 -0400

    hi

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+"""
+Filename: interview.py
+Author: <Hernandez perdones, Jose>
+Created: <8/27/2026>
+Instructor: Mr.burgess
+
+"""
 
 
-print("Hello World")
+print("hello i am the interviewer and also the host i gonna to tell you some question")
 
-
-
+x=input("number 1 who the first person that invited Marshmallow very simple the marshmallow to eat but hard to find the creator")
+print(x)
+x=input("number 2 who the first mob in  Minecraft  there like million of mob just see Minecraft history to find out")
+print(x)
+x=input("number 3 can you predict battle of dream island:tpot this 25 ep i think can you predicted what happen")
+print(x)
+x=input("number 4 this is a truth or dare question be ready what game is better  Minecraft  or roblox or other ")
+print(x)
+x=input("number 5 do ai is evil or not that gonna to be a honestly choice my good friend")
+print(x)
+print("that just it you decide what is the real answer goodbye")
