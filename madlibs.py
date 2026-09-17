146329ec4ec696d28feea30e175c9952b6cee367
jose108176 <jose108176@mcpsva.org>
2026-09-17 11:01:42 -0400

    hi

--- madlibs.py
+++ madlibs.py
@@ -9,11 +9,11 @@ Instructor: Burgess
 
 Adjective=input("adjective:Blue ")
 Noun=input("noun:card")
-Plural_Noun=input("pluar noun")
-Verb_1=input("verb 1: ")
-Place=input("place: ")
-Adjective=input("adjective: ")
-Body_Part=input("body part: ")
+Plural_Noun=input("pluar noun:sandwiches")
+Verb_1=input("verb 1:fixing ")
+Place=input("place:a pizzeria ")
+Adjective=input("adjective 2:smells delicious ")
+Body_Part=input("body part:head ")
 Noun=input("noun: ")
 Exclamation_Yell=input("exclamation yell: ")
 Animal=input("animal: ")
@@ -28,22 +28,19 @@ Noun=input("noun: ")
 Verb=input("verb: ")
 Adjective=input("adjective: ")
 
-
- The Story: Lost in Deep Space
-Captain Star-Crasher woke up with a [1. Adjective] headache. Looking out the view-screen of the USS [2. Noun], they realized the ship was completely surrounded by space [3. Plural Noun].
-"Engage the hyperdrive!" the Captain ordered, but the engineer was too busy [4. Verb ending in -ing] to notice. Suddenly, a wormhole opened up and sucked them all the way to [5. Place].
-The planet below looked incredibly [6. Adjective]. To make matters worse, the ship's main radar terminal began to sprout a giant [7. Body Part] and leaked a sticky pool of [8. Noun] all over the dashboard.
-"[9. Exclamation/Yell]!" shouted the co-pilot, who happened to be an intelligent, talking [10. Animal].
-Without thinking, the crew [11. Verb past tense] out of their seats and splashed into a nearby crater filled with [12. Liquid]. Local aliens, who resembled glowing [13. Plural Noun], gathered around. Their leader was remarkably [14. Adjective] and possessed exactly [15. A Number] eyes.
-"We demand your [16. Color] spaceship," the leader declared in a [17. Silly Word] voice. "Unless you give us your ultimate weapon: a holy [18. Noun]!"
-The crew didn't want to [19. Verb], but they had no choice. Handing it over saved the day, and they celebrated their [20. Adjective] escape into the cosmos.
-
-
-
-
-
-
-
-
-
-
+print("The Story: Lost in Deep Space")
+print("Captain Star-Crasher woke up with a [1. Adjective] headache.")
+print("Looking out the view-screen of the USS [2. Noun],they realized the ship was completely surrounded by space [3. Plural Noun].")
+print("Engage the hyperdrive_the Captain ordered, but the engineer was too busy [4.Verb ending in -ing] to notice")
+print("Suddenly, a wormhole opened up and sucked them all the way to [5. Place].")
+print("The planet below looked incredibly [6. Adjective].")
+print("To make matters worse, the ship's main radar terminal began to sprout a giant [7. Body Part]")
+print("and leaked a sticky pool of [8. Noun] all over the dashboard.")
+print("[9. Exclamation/Yell]shouted the co-pilot,who happened to be an intelligent, talking [10. Animal].")
+print("Without thinking, the crew [11. Verb past tense]out of their seats and splashed into a nearby crater filled with [12. Liquid].")
+print("Local aliens, who resembled glowing)[13. Plural Noun], gathered around.")
+print("Their leader was remarkably [14. Adjective] and possessed exactly [15. A Number] eyes.")
+print("We demand your [16. Color] spaceship,the leader declared in a [17. Silly Word] voice.")
+print("Unless you give us your ultimate weapon: a holy [18. Noun]!.")
+print("The crew didn't want to [19. Verb],but they had no choice.")
+print("Handing it over saved the day, and they celebrated their [20. Adjective] escape into the cosmos.")
